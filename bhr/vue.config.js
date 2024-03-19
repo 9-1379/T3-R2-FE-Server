17b78b7ff0bfcd7c8807d80df186d6a520ef1603
yjin_note <yjinlee99@naver.com>
2024-03-19 14:35:34 +0900

    spring, vue 연동

--- bhr/vue.config.js
+++ bhr/vue.config.js
@@ -2,3 +2,13 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  // npm run build 타겟 디렉토리
+  outputDir: '../../T3-R1-BE-Server/bhr/src/main/resources/static',
+
+  // npm run serve 개발 진행시에 포트가 다르기때문에 프록시 설정
+  devServer: {
+    proxy: 'http://localhost:8080'
+  }
+};
