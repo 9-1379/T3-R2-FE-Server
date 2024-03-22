d89fc3668be003da7eb2f6c790efb62e962ed7fb
leedonghyuk <ldh0308@naver.com>
2024-03-22 09:40:19 +0900

    [Modify} vue.config.js 문법수정

--- bhr/vue.config.js
+++ bhr/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
-    allowedHosts: "all",
-    transpileDependencies: true
-  }
+    allowedHosts: ["all"],
+  },
+  transpileDependencies: true
 })
