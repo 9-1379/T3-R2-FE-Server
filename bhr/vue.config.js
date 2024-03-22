a160b90e7db924ed306856246db7c218cf7c1069
leedonghyuk <ldh0308@naver.com>
2024-03-22 11:05:37 +0900

    [Modify] port default

--- bhr/vue.config.js
+++ bhr/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
     allowedHosts: ["all"],
+    port: 3000
   },
   transpileDependencies: true
 })
