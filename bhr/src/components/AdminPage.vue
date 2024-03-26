14b62a0cacf26666d4ac8ef2653fe48e7ed7907a
leedonghyuk <ldh0308@naver.com>
2024-03-26 12:04:29 +0900

    [Add]차트 추가

--- bhr/src/components/AdminPage.vue
+++ bhr/src/components/AdminPage.vue
@@ -3,6 +3,9 @@
   <div style="width: 30%;">
     <canvas id="deptNameChart"></canvas>
   </div>
+  <div style="width: 30%;">
+    <canvas id="deptNameBarChart"></canvas> <!-- 막대 차트 추가 -->
+  </div>
   <div>
     <p>Total Employees: {{ count }}</p>
     <ul>
@@ -31,26 +34,29 @@ export default {
         const response = await axios.get('http://localhost:8000/list');
         employeeList.value = response.data.employeeList;
         count.value = response.data.count;
-        createChart(employeeList.value);
+        const departmentsData = createChartData(employeeList.value);
+        createChart(departmentsData)
+        createBarChart(departmentsData);
       } catch (error) {
         console.error('Error fetching users:', error);
         // 에러 처리 로직
       }
     });
 
-    function createChart(employees) {
-      const departments = employees.reduce((acc, cur) => {
+    function createChartData(employees) {
+      return employees.reduce((acc, cur) => {
         acc[cur.deptName] = (acc[cur.deptName] || 0) + 1;
         return acc;
       }, {});
-
+    }
+    function createChart(departmentsData){
       const ctx = document.getElementById('deptNameChart').getContext('2d');
       new Chart(ctx, {
         type: 'pie',
         data: {
-          labels: Object.keys(departments),
+          labels: Object.keys(departmentsData),
           datasets: [{
-            data: Object.values(departments),
+            data: Object.values(departmentsData),
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -71,6 +77,31 @@ export default {
           }]
         },
         options: {
+          responsive: true,
+        }
+      });
+    }
+    // 막대 차트 생성 함수
+    function createBarChart(departmentsData) {
+      const ctx = document.getElementById('deptNameBarChart').getContext('2d');
+      new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: Object.keys(departmentsData),
+          datasets: [{
+            label: '부서별 직원 수',
+            data: Object.values(departmentsData),
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
           responsive: true,
         }
       });
