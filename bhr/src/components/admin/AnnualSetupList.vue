12d1e7004091f45ff0723495f554910460a5cddc
leedonghyuk <ldh0308@naver.com>
2024-04-04 11:38:14 +0900

    [Modify]수정중

--- bhr/src/components/admin/AnnualSetupList.vue
+++ bhr/src/components/admin/AnnualSetupList.vue
@@ -1,17 +1,67 @@
 <template>
   <div>
-    <AdminMenuBar />
-    
+    <AdminMenu />
+    <div>
+      <h1>직원 연차 부여</h1>
+      해당 년도: {{ currentYear }}
+      <input v-model="form.annualTotal" />
+      <button @click="newAnnualTotal">부여</button>
+    </div>
+    <div>
+      <h1>직원 연차 현황</h1>
+      <table v-for="(annual, index) in annualStatus" v-bind:key="index">
+        {{
+          annual.name
+        }}
+        -
+        {{ annual.annualTotal }}
+        -
+        {{ annual.annualCnt }}
+      </table>
+      <!-- <table border="1">
+        <thead>
+          <th>월</th>
+          <th v-for="(month, index) in months" v-bind:key="index"></th>
+        </thead>
+      </table> -->
+    </div>
   </div>
 </template>
 
 <script>
-import AdminMenuBar from '@/components/menu/AdminMenu.vue';
+import AdminMenu from "@/components/menu/AdminMenu.vue";
+import axiosInstance from "@/axios";
 
 export default {
   components: {
-    AdminMenuBar,
-  }
-}
-
+    AdminMenu,
+  },
+  data() {
+    return {
+      form: {
+        annualYear: "",
+        annualTotal: "",
+      },
+      currentYear: new Date().getFullYear(),
+      annualStatus: [],
+    };
+  },
+  methods: {
+    async newAnnualTotal() {
+      await axiosInstance.post("/annualTotal", this.form);
+    },
+    async fetchAnnualStatus() {
+      const statusResponse = await axiosInstance.get(
+        `/status/${this.currentYear}`
+      );
+      this.annualStatus = statusResponse.data;
+    },
+  },
+  mounted() {
+    this.fetchAnnualStatus();
+  },
+};
 </script>
+
+<style scoped>
+</style>
