1bd8c1c9b3e245fdcb7507213b9648c889628775
leedonghyuk <ldh0308@naver.com>
2024-04-09 14:12:43 +0900

    [Modify]axios 수정

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://ec2-43-202-6-5.ap-northeast-2.compute.amazonaws.com:8000',
+    baseURL: 'https://9-1379-be.store/',
 });
 
 axiosInstance.interceptors.request.use(
